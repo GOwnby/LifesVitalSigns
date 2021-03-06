09670f29b7fa52c915109c95ca480df7b8d122cf
Ethreil <gracenownby@gmail.com>
2021-03-05 20:10:21 -0600

    debugging

--- researcher/write_data_final.py
+++ researcher/write_data_final.py
@@ -208,30 +208,14 @@ def write_N2O():
     try:
         fp = open('data/N2OData.txt')
         linesInData = 0
-        for line in fp:
-            linesInData += 1
-            if not(re.match('#', line)):                    
-                if re.match(patterns[0], line):
-                    match_year = re.match(patterns[0], line)
-                    lineEdited = line.replace(str(match_year.group(0)), '')
-                    lineEdited = lineEdited[7:]
-                    match_ppb1 = re.match(patterns[1], lineEdited)
-                    if (str(match_ppb1.group(0)) != 'nan'):
-                        lineEdited = lineEdited.replace(str(match_ppb1.group(0)), '')
-                        match_ppb2 = re.match(patterns[2], lineEdited)
-                        this_year = str(match_year.group(0))
-                        this_ppb = str(match_ppb1.group(0)) + str(match_ppb2.group(0))
-                        counter = 1
-                        while counter <= 12:
-                            this_year = this_year + '_' + str(counter)
-                            data[this_year] = float(this_ppb)
-                            counter =+ 1
+        dataObject = function_write(data, fp, patterns, linesInData)
+        linesInData = dataObject[1]
         fp.close()
 
         basepath = os.path.dirname(__file__)
         filePath = os.path.abspath(os.path.join(basepath, "..", "LifesVitalSigns/static/static_dirs/js/json/N2OData.json"))
         outfile = open(filePath, 'w')
-        json.dump(data, outfile)
+        json.dump(dataObject[0], outfile)
         learn_data.average_dataset('N2O')
         data = None
         outfile = open('data/N2ODataLines.txt', 'w')
@@ -530,8 +514,6 @@ def write_All():
         os.makedirs(path)
     write_CO2()
     time.sleep(5)
-    write_N2O()
-    time.sleep(5)
     write_CH4()
     time.sleep(5)
     write_CFC11()
@@ -539,3 +521,5 @@ def write_All():
     write_CFC12()
     time.sleep(5)
     write_Temperature()
+    write_N2O()
+    time.sleep(5)
